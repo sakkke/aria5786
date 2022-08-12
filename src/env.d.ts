1f61f60035e8a51075353f8c28c94938973f522f
sakkke <w32w64@gmail.com>
2022-08-12 18:10:54 +0900

    :technologist: add .env.development

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_POCKETBASE_REDIRECT_URL: string;
+  readonly PUBLIC_POCKETBASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
